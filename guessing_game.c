fd626f443199c4d6fe5b34396cfcd8ee44785ada
hip002308 <t_yamashita@hip-pro.info>
2024-04-22 10:43:16 +0900

    表記の調整等

--- guessing_game.c
+++ guessing_game.c
@@ -2,38 +2,47 @@
 #include <stdlib.h>
 #include <time.h>
 
+// 正解値の最小値・最大値
+#define ANSWER_MIN (1)
+#define ANSWER_MAX (100)
+
 int main(void) {
-	char input[10] = { '\0' };
-	int answer_player;
-	int correct_answer;
-	int count = 0;
+  // プレイヤーの入力保持
+  char input[10] = { '\0' };
+  // プレイヤーの解答
+  int player_answer;
+  // ゲームの正解
+  int correct_answer;
+  // プレイヤーの解答回数
+  int count = 0;
 
-	// 答えの数値をランダムで設定する
-	srand((unsigned) time (NULL));
-	correct_answer = rand() % 100 + 1;
+  // 正解の数値を生成
+  srand((unsigned) time(NULL));
+  correct_answer = rand() % ANSWER_MAX + ANSWER_MIN;
 
-	do {
-		printf("1から100までの数字を入力してください。：");
-		scanf_s("%9s", input, (unsigned) (sizeof(input) / sizeof(input[0])));
-		answer_player = atoi(input);
-		if ((answer_player < 1) || (answer_player > 100)) {
-			printf("\a【注意】1以上100以下の数字を入力してください。\n");
-			continue;
-		}
+  do {
+    // プレイヤー入力
+    printf("1から100までの数字を入力してください：");
+    scanf_s("%9s", input, (unsigned) (sizeof(input) / sizeof(input[0])));
+    player_answer = atoi(input);
+    if ((player_answer < ANSWER_MIN) || (player_answer > ANSWER_MAX)) {
+      printf("\a【注意】1以上100以下の数字を入力してください。\n\n");
+      continue;
+    }
+    count++;
 
-		if (answer_player < correct_answer) {
-			puts("Small");
-		}
-		else if (answer_player > correct_answer) {
-			puts("Big");
-		}
-		else {
-			puts("Bingo!");
-		}
-		count++;
-	} while (answer_player != correct_answer);
+    // 正誤判定・表示
+    printf("あなたの解答\"%d\"は", player_answer);
+    if (player_answer < correct_answer) {
+      puts("Small");
+    } else if (player_answer > correct_answer) {
+      puts("Big");
+    } else {
+      puts("Bingo!");
+      printf("あなたは%d回目で正解しました。", count);
+    }
+    printf("\n");
+  } while (player_answer != correct_answer);
 
-	printf("あなたは%d回目で正解しました", count);
-
-	return 0;
+  return 0;
 }
