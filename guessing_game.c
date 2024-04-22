39c6f03267ddb437924af1984879943b23e7efca
hip002308 <t_yamashita@hip-pro.info>
2024-04-22 16:10:42 +0900

    正誤判定の冗長問題を妥協した

--- guessing_game.c
+++ guessing_game.c
@@ -6,9 +6,7 @@
 #define ANSWER_MIN (1)
 #define ANSWER_MAX (100)
 
-#define TRUE 1
-
-// 入力バッファを読み飛ばす
+// 残りの入力バッファを読み飛ばす
 void ThroughRestBuffer() {
   char buffer = getchar();
   while (buffer != '\n') {
@@ -17,7 +15,7 @@ void ThroughRestBuffer() {
 }
 
 // 数字の文字列を数値にして返す(先頭が文字列の場合、ANSWER_MIN - 1を返す)
-int AtoiWithException(char str[]) {
+int AtoiWithinRange(char str[]) {
   int ret = atoi(str);
   if (ret == 0 && str[0] != '0') {
     ret = ANSWER_MIN - 1;
@@ -27,39 +25,18 @@ int AtoiWithException(char str[]) {
 
 // 答えの入力を求める
 void AnswerByPlayer(int* player_answer) {
-  char player_answer_input[10] = {'\0'};
-  while (TRUE) {
+  char answer_input[10] = {'\0'};
+  while (1) {
     printf("%dから%dまでの数字を入力してください：", ANSWER_MIN, ANSWER_MAX);
-    // バッファサイズに最大文字数を返す_countofマクロを使用
-    scanf_s("%s", player_answer_input, (unsigned)_countof(player_answer_input));
+    // 第3引数のバッファサイズに、配列の最大文字数を返す_countofマクロを使用
+    scanf_s("%9s", answer_input, (unsigned)_countof(answer_input));
     ThroughRestBuffer();
-    *player_answer = AtoiWithException(player_answer_input);
+    *player_answer = AtoiWithinRange(answer_input);
     if ((*player_answer >= ANSWER_MIN) && (*player_answer <= ANSWER_MAX)) {
       break;
     }
     printf("\a【注意】%d以上%d以下の数字を入力してください。\n\n", ANSWER_MIN, ANSWER_MAX);
   }
-}
-
-// 判定結果を表示（引数が正ならBig、負ならSmall、0ならBingo）
-void PrintCheckResult(int diff) {
-  if (diff > 0) {
-    printf("Big\n");
-  }
-  else if (diff < 0) {
-    printf("Small\n");
-  }
-  else {
-    printf("Bingo!\n");
-  }
-  printf("\n");
-}
-
-// 正誤判定をする（戻り値：0は正解、0以外は不正解）
-int CheckAnswer(int player_answer, int correct_answer) {
-  int ret = player_answer - correct_answer;
-  PrintCheckResult(ret);
-  return ret;
 }
 
 // 答えを生成する
@@ -80,16 +57,27 @@ int main(void) {
   correct_answer = GenerateAnswer();
 
   printf("数当てゲームです。\n");
-  do {
+  while (1) {
     // プレイヤー入力
     AnswerByPlayer(&player_answer);
     answer_count++;
 
     // 正誤判定・表示
     printf("あなたの解答\"%d\"は", player_answer);
-  } while (CheckAnswer(player_answer, correct_answer));
-
-  printf("あなたは%d回目で正解しました。\n", answer_count);
+    int diff = player_answer - correct_answer;
+    if (diff > 0) {
+      printf("Big\n");
+    }
+    else if (diff < 0) {
+      printf("Small\n");
+    }
+    else {
+      printf("Bingo!\n");
+      printf("あなたは%d回目で正解しました。\n", answer_count);
+      break;
+    }
+    printf("\n");
+  }
 
   return 0;
 }
